aeeb30d72697ecd8660599b15492e8fcaa6294f9
Vibha2698 <52655578+Vibha2698@users.noreply.github.com>
2019-07-30 11:57:06 +0530

    Update assg26.py

--- assg26.py
+++ assg26.py
@@ -26,3 +26,66 @@ def index1():
         return "fail"
 if __name__=="__main__":
     app.run()
+    
+    "HTML CODE"
+    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>GAME TIME</title>
+</head>
+<body>
+
+<form name="myform" style="position:absolute;top:25%;left:40%;">
+    <h1>KLE INSTITUTE OF TECHNOLOGY</h1>
+    <h1>HUBBALLI</h1>
+</form>
+
+
+
+
+<form name="myform"   method="post" >
+            <div style="position:absolute;top:45%;left:10%;">
+No of Lines:<p><input type="number"name="LINES" id="lines" style="font-size:18pt;height=35px;width=200px;"></p>
+No of Words:<p><input type="number"name="WORDS" id="words" style="font-size:18pt;height=35px;width=200px;"></p>
+No of Characters:<p><input type="number"name="CHARACTERS" id="char" style="font-size:18pt;height=35px;width=200px;"></p></div>
+          <div style="position:absolute; bottom:10%;right:45%; font-size:40pt;">
+        <input type="submit"  formaction="/submit" name="submit" id="submit" value="SUBMIT">
+            </div>
+
+<div id="timer" style="position:absolute; top:5%;right:5%; font-size:40pt; color:red;"></div>
+
+<script type="text/javascript">
+
+var timeoutHandle;
+function countdown(minutes,seconds){
+ function tick(){
+  var counter=document.getElementById("timer");
+
+  counter.innerHTML=minutes.toString()+":" + (seconds<10 ? "0" : "")+ String(seconds);
+  seconds--;
+  if(seconds>=0) {
+  timeoutHandle=setTimeout(tick,1000);
+  }else {
+   if(minutes>=1) {
+   setTimeout(function() {
+   countdown(minutes-1,59);
+   }, 1000);
+   }
+  }
+ }
+ tick();
+ }
+ countdown(1,30);
+var auto_refresh=setInterval(function(){
+submitform();
+},90000);
+
+function submitform() {
+document.getElementById("submit").click();
+}
+</script>
+</form>
+</body>
+</html>
